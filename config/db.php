cbf495e1dd4de8cfaf4a897eb39538f95a20c669
berty <lucho.2012.tandil@gmail.com>
2022-01-27 15:22:54 -0300

    Se quitan algunas de las migraciones

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=precios',
-    'username' => 'root',
-    'password' => 'LuchO',
+    'dsn' => 'mysql:host=localhost;dbname=reparaciones',
+    'username' => 'reparaciones',
+    'password' => 'UyKm2^Dj1YJbXR#zObN^whuX!K6!f$F$VvpOIu&e!9Uu1iYxsX',
     'charset' => 'utf8',
 ];
